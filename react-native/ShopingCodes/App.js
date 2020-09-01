94bde74cc2fdd014e23d7c8473b6ad6aa9047242
Wantonius <hentonen@gmail.com>
2020-09-01 10:10:23 +0300

    shopping app

---
+++ react-native/ShopingCodes/App.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ShoppingForm from './ShoppingForm'
+
+export default class App extends React.Component {
+  
+  constructor(props) {
+	  super(props);
+	  this.state= {
+		  list:[],
+		  id:100
+	  }
+  }
+  
+  addToList = (item) => {
+	  item.id = this.state.id;
+	  let tempList = this.state.list.concat(item);
+	  this.setState({
+		  id:item.id+1,
+		  list:tempList
+	  })
+  }
+  
+  removeFromList = (id) => {
+	  let tempId = parseInt(id,10);
+	  let tempList = this.state.list.filter(item => item.id !== tempId);
+	  this.setState({
+		  list:tempList
+	  })
+  }
+  
+  render() {
+	  return (
+		<View style={styles.container}>
+			<ShoppingForm addToList={this.addToList}/>
+		</View>
+	  );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
